1abbc5ea70b9c4492a0a57b3c8fd4fcbb0f3f3da
Albambinou <101211883+Albambinou@users.noreply.github.com>
2024-12-10 16:57:01 +0100

    Update Clavier.ino

--- Clavier.ino
+++ Clavier.ino
@@ -17,6 +17,19 @@ char keys[ROWS] [COLS] =
 byte rowPins[ROWS] = {8, 9, 10, 11};
 byte colPins[COLS] = {3, 4, 5, 6};
 
+// Mot de passe :
+char pe =  'E';
+char de =  'B';
+char te =  '6';
+char qe =  '9';
+
+char key1;
+char key2;
+char key3;
+char key4;
+
+int securite = 0;
+
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup()
@@ -26,64 +39,52 @@ Serial.begin(9600);
 
 void loop()
 {
-char key = keypad.getKey();
-if (key)
-{
-  if (key == '4')
-  {
-    analogWrite(led_B, 255);
-    char key2 = 0;
-
-    while (!key2)
-    {
-      key2 = keypad.getKey();
-    }
-
-    if (key2 == '2')
-    {
-      analogWrite(led_B, 0);
-      analogWrite(led_V, 255);
-    
-    key2 = 0;
-    while (!key2)
+while (!key1)
+      {
+        key1 = keypad.getKey();
+      }
+while (!key2)
       {
         key2 = keypad.getKey();
-      } 
-      if (key2 == 'F')
+      }  
+while (!key3)
       {
-      Serial.println("La touche F a été préssée !, vous pouvez entrer");
-      analogWrite(led_B, 255);
-      analogWrite(led_V, 255);
-      delay(500);
-      analogWrite(led_V, 0); 
-      analogWrite(led_B, 0);
-      }
-      else
-    {
-      Serial.println("faux");
-      analogWrite(led_B, 0);
-      analogWrite(led_V, 0);
-      analogWrite(led_R, 255);
-      delay(500);
-      analogWrite(led_R, 0);
-    }
-    }
-    else
-    {
-      Serial.println("faux");
-      analogWrite(led_B, 0);
-      analogWrite(led_V, 0);
-      analogWrite(led_R, 255);
-      delay(500);
-      analogWrite(led_R, 0);
-    }
-  }
-  else
-  {
-    Serial.print("La touche ");
-    Serial.print (key);
-    Serial.println(" à été préssée");
-    Serial.println();
-  }
+        key3 = keypad.getKey();
+      } 
+while (!key4)
+      {
+       key4 = keypad.getKey();
+      } 
+
+if(key1 == pe && key2 == de && key3 == te && key4 == qe)
+{
+  analogWrite(led_V, 255);
+  delay(1000);
+  analogWrite(led_V, 0);
+  
+  key1 = 0;
+  key2 = 0;
+  key3 = 0;
+  key4 = 0;
+  securite = 0;
 }
+else
+{
+  analogWrite(led_R, 255);
+  delay(500);
+  analogWrite(led_R, 0);
+  
+  key1 = 0;
+  key2 = 0;
+  key3 = 0;
+  key4 = 0;
+  securite++;
+}
+  if (securite == 3)
+  {
+    analogWrite(led_R, 255);
+    delay(5000);
+    analogWrite(led_R, 0);
+    securite = 0;
+  }
 }
